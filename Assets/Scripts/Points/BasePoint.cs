39fc3c091950e156eb3298d7a664cee6a0e46c23
Rosgard <roseguardd@gmail.com>
2019-06-14 10:16:38 +0300

    + Added points connection and findway from one point to next ones

---
+++ Assets/Scripts/Points/BasePoint.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePoint : MonoBehaviour
+{
+    public List<GameObject> NextPoints = new List<GameObject>();
+    public List<GameObject> PrevPoints = new List<GameObject>();
+    public int CurrentId = ++m_lastCellId;
+
+    static private int m_lastCellId = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CurrentId == 1)
+        {
+            List<GameObject> list = new List<GameObject>();
+            Debug.Log(FindWay(6, 4, list));
+            foreach(var l in list)
+                Debug.Log(l.name);
+            Debug.Log("===");
+            list.Clear();
+
+            Debug.Log(FindWay(9, 4, list));
+            foreach (var l in list)
+                Debug.Log(l.name);
+            Debug.Log("===");
+            list.Clear();
+
+            Debug.Log(FindWay(5, 4, list));
+            foreach (var l in list)
+                Debug.Log(l.name);
+            Debug.Log("===");
+            list.Clear();
+
+            Debug.Log(FindWay(5, 3, list));
+            foreach (var l in list)
+                Debug.Log(l.name);
+            Debug.Log("===");
+            list.Clear();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool FindWay(int toPointID, int stepsLimitation, List<GameObject> outputWay)
+    {
+        if(stepsLimitation == 0)
+        {
+            return false;
+        }
+        outputWay.Add(gameObject);
+        if(toPointID == CurrentId)
+        {
+            return true;
+        }
+        else if(stepsLimitation < 2)
+        {
+            outputWay.Clear();
+            return false;
+        }
+        for (int i = 0; i < NextPoints.Count; i++)
+        {
+            List<GameObject> tempPointList = new List<GameObject>();
+            if (NextPoints[i].GetComponent<BasePoint>().FindWay(toPointID, stepsLimitation - 1, tempPointList))
+            {
+                outputWay.AddRange(tempPointList);
+                return true;
+            }
+        }
+        return false;
+    }
+}
