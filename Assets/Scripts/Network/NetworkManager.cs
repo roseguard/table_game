61345f2f7ecb194ad727376e2323b2b28ea9e71f
Rosgard <roseguardd@gmail.com>
2019-08-04 23:35:17 +0300

    + Network manager and networkunit preparation

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// request rules <request size>&<request type>&<(propname)=(propvalue)...>
+class NetworkManager : MonoBehaviour
+{
+    public bool IsServer = false;
+    public bool UseLocalHost = false;
+    public int ServerPost = 21034;
+    public int MaxPlayers = 12;
+    private Socket m_listener = null;
+
+    public enum Requests
+    {
+        SyncPlayers,
+        SyncPlayerCards,
+        SyncPlayerDices,
+    }
+
+    public void Start()
+    {
+        if (IsServer)
+        {
+            HostServer();
+        }
+    }
+
+    private void HostServer()
+    {
+        IPHostEntry iPHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr;
+        Debug.Log(iPHost.AddressList[0]);
+        if (UseLocalHost)
+        {
+            ipAddr = IPAddress.Loopback;
+        }
+        else
+        {
+            ipAddr = IPAddress.Loopback;
+        }
+        Debug.Log(ipAddr);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, ServerPost);
+
+        m_listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        m_listener.Bind(localEndPoint);
+
+        m_listener.Listen(MaxPlayers);
+
+        Debug.Log("Hosted");
+
+        var e = new SocketAsyncEventArgs();
+        bool sockEnable = m_listener.AcceptAsync(e);
+        e.Completed += E_Completed;
+    }
+
+    private void ConnectToServer()
+    {
+
+    }
+
+    void Update()
+    {
+        if (IsServer && m_listener != null)
+        {
+        }
+    }
+    NetworkUnit newUnit;
+    private void E_Completed(object sender, SocketAsyncEventArgs e)
+    {
+        var b = new SocketAsyncEventArgs();
+        bool sockEnable = m_listener.AcceptAsync(b);
+        b.Completed += E_Completed;
+
+        newUnit = new NetworkUnit(e.AcceptSocket);
+        newUnit.SendMessage("shit\n\n");
+    }
+}
---
+++ Assets/Scripts/Network/NetworkUnit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class NetworkUnit
+{
+    private Socket m_socket = null;
+    private SocketAsyncEventArgs m_receiveEvent = new SocketAsyncEventArgs();
+
+    public NetworkUnit(Socket socket)
+    {
+        m_socket = socket;
+
+        m_receiveEvent.SetBuffer(new byte[1024], 0, 1024);
+
+        m_socket.ReceiveAsync(m_receiveEvent);
+        m_receiveEvent.Completed += MessageReceived;
+    }
+
+    public void MessageReceived(object sender, SocketAsyncEventArgs e)
+    {
+        Debug.Log(e.Buffer.ToString());
+
+        m_socket.ReceiveAsync(m_receiveEvent);
+        m_receiveEvent.Completed += MessageReceived;
+    }
+
+    public void SendMessage(string message)
+    {
+        SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+        e.SetBuffer(Encoding.UTF8.GetBytes(message), 0, message.Length);
+        m_socket.SendAsync(e);
+    }
+}
