7d2f0c95401f4199d5a754568774a71216f8e0cf
Rosgard <roseguardd@gmail.com>
2019-08-11 18:00:55 +0300

    + receiving part of protocol (mess size)

--- Assets/Scripts/Network/NetworkUnit.cs
+++ Assets/Scripts/Network/NetworkUnit.cs
@@ -7,25 +7,51 @@ using UnityEngine;
 
 public class NetworkUnit
 {
+    //  <4>             <4>         <4>            <N>
+    //  <size>          <messid>    <messcommand>  <params>
+
     private Socket m_socket = null;
+    private const int m_bufferSize = 255;
+    private byte[] m_buffer = new byte[m_bufferSize];
     private SocketAsyncEventArgs m_receiveEvent = new SocketAsyncEventArgs();
 
     public NetworkUnit(Socket socket)
     {
         m_socket = socket;
 
-        m_receiveEvent.SetBuffer(new byte[1024], 0, 1024);
+        m_receiveEvent.SetBuffer(m_buffer, 0, m_bufferSize);
 
         m_socket.ReceiveAsync(m_receiveEvent);
-        m_receiveEvent.Completed += MessageReceived;
+        m_receiveEvent.Completed += M_receiveEvent_Completed;
     }
 
-    public void MessageReceived(object sender, SocketAsyncEventArgs e)
+    private void M_receiveEvent_Completed(object sender, SocketAsyncEventArgs e)
     {
-        Debug.Log(e.Buffer.ToString());
+        string tempStr = System.Text.Encoding.UTF8.GetString(e.Buffer);
+        if(tempStr.Length > 12)
+        {
+            Array.Clear(e.Buffer, 0, m_bufferSize);
+            string paramsizestr = new string(tempStr.ToCharArray(), 0, 4);
+            string messidstr = new string(tempStr.ToCharArray(), 4, 4);
+            string messcommandstr = new string(tempStr.ToCharArray(), 8, 4);
+
+            UInt32 paramsize = CastCharToInt(paramsizestr.ToCharArray());
+            UInt32 messid = CastCharToInt(messidstr.ToCharArray());
+            UInt32 messcommand = CastCharToInt(messcommandstr.ToCharArray());
+
+            Debug.Log("AAAAAAAAA");
+            Debug.Log(paramsize);
+            Debug.Log(messid);
+            Debug.Log(messcommand);
+        }
+        
 
         m_socket.ReceiveAsync(m_receiveEvent);
-        m_receiveEvent.Completed += MessageReceived;
+    }
+
+    private UInt32 CastCharToInt(char[] bytes)
+    {
+        return ((UInt32)bytes[0]) | ((UInt32)bytes[1] << 8) | ((UInt32)bytes[2] << 16) | ((UInt32)bytes[3] << 24);
     }
 
     public void SendMessage(string message)
